876eaffad99a9695fa6fd22acf0ecc11000ea4bb
Kashirskiy Nikita <lordwebkit@gmail.com>
2023-04-15 20:20:07 +0300

    Add montserrat fonts global

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap");
+
 html {
   font-size: 10px;
 }
@@ -18,5 +20,6 @@ html {
   font-size: 1.4rem;
   padding: 0;
   margin: 0;
+  font-family: Montserrat, sans-serif;
 }
 </style>
