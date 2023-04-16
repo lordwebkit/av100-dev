da8b5bf38ccbdd9b0217272f6e0884b6e97d72ca
Kashirskiy Nikita <lordwebkit@gmail.com>
2023-04-16 22:45:07 +0300

    create setters for userData

--- src/stores/user.js
+++ src/stores/user.js
@@ -9,8 +9,8 @@ const deviceOS = 'windows';
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    authData: {},
-    userData: JSON.parse(localStorage.getItem('userData')) || {}
+    authData: JSON.parse(localStorage.getItem('authData')) || {},
+    userData: {}
   }),
   getters: {
     getUserData: (state) => state.userData,
@@ -27,7 +27,8 @@ export const useUserStore = defineStore('user', {
           };
           const response = await axios.post(loginUrl, auth, { headers });
           this.authData = response.data;
-          this.setUserData()
+          localStorage.setItem('authData', JSON.stringify(response.data))
+          router.push('/settings')
         } catch (err) {
           console.error(err);
         }
@@ -43,11 +44,44 @@ export const useUserStore = defineStore('user', {
         };
         const response = await axios.get(`${getUserUrl}${this.authData.user?.id}`, { headers });
         this.userData = response.data;
-        localStorage.setItem('userData', JSON.stringify(response.data))
-        router.push('/settings')
       } catch (err) {
         console.error(err);
       }
+    },
+    async sendUserData() {
+      try {
+        const headers = {
+          'Accept': 'application/json',
+          'X-Api-Key': apiKey,
+          'X-User-Token': this.authData.token,
+          'X-Device-OS': deviceOS
+        }
+        const response = await axios.put(`${getUserUrl}${this.authData.user?.id}`, this.userData, { headers })
+        console.log(response)
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    setColorLenta() {
+      this.userData.colorlenta = !this.userData.colorlenta
+    },
+    setLockLentaUpdate() {
+      this.userData.locklentaupdate = !this.userData.locklentaupdate
+    },
+    setTimezone(e) {
+      this.userData.timezonestring = e.target.value
+    },
+    setRedirectTarget(e) {
+      this.userData.redirecttarget = Number(e.target.value)
+    },
+    setNotifyType(e) {
+      this.userData.redirecttarget = e.target.value
+    },
+    setLastName(e) {
+      this.userData.lname = e.target.value
+    },
+    setPhone(e) {
+      this.userData.phone = e.target.value
     }
   },
 });
